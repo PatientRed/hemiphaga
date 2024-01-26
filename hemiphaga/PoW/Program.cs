48eb5f70521ad86b0aa20b35572337e779824ebe
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:46:55 +0300

    Console PoW implemented

---
+++ hemiphaga/PoW/Program.cs
@@ -0,0 +1,21 @@
+﻿using hemiphaga;
+
+namespace PoW
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var shape in ExternalDataProcessor.Process(ExternalDataProcessor.Receive()))
+            {
+                Console.WriteLine($"Figure: {shape.GetType().Name}");
+                Console.WriteLine($"Area: {shape.CalculateArea()}");
+                if (shape is Triangle)
+                    Console.WriteLine($"Triangle is right: {(shape as Triangle).IsRight}");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
