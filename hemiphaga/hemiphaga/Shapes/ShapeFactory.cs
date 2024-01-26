4799e899a0bd5bdd8fc7174817b9c1a02fc052ad
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:31:23 +0300

    ShapeFactory implemented

---
+++ hemiphaga/hemiphaga/Shapes/ShapeFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace hemiphaga
+{
+    public static class ShapeFactory
+    {
+        public static IShape CreateShape(string type, params double[] values)
+        {
+            return type switch
+            {
+                nameof(Circle) => CreateCircle(values[0]),
+                nameof(Triangle) => CreateTriangle(values),
+                _ => throw new InvalidOperationException("No shape found."),
+            };
+        }
+
+        private static Circle CreateCircle(double radius) => new Circle(radius);
+        private static Triangle CreateTriangle(params double[] dimensions) => new Triangle(dimensions[0], dimensions[1], dimensions[2]);
+    }
+}
