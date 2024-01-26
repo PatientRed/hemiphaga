9689e1bd270ece79853f687b5dc4397882b52a5d
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:26:54 +0300

    Circle implemented

---
+++ hemiphaga/hemiphaga/Shapes/Circle.cs
@@ -0,0 +1,9 @@
+﻿namespace hemiphaga
+{
+    public class Circle(double radius) : IShape
+    {
+        private readonly double _radius = radius;
+
+        public double CalculateArea() => Math.PI * _radius * _radius;
+    }
+}
