52f4c846fae2928a12ebff9e42f3951ba8d73221
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:46:26 +0300

    Dummy external data source receiver & processor added

---
+++ hemiphaga/PoW/ExternalDataProcessor.cs
@@ -0,0 +1,20 @@
+﻿using hemiphaga;
+using PoW.MyShapes;
+using System.Globalization;
+
+namespace PoW
+{
+    internal static class ExternalDataProcessor
+    {
+        public static IEnumerable<string> Receive() => File.ReadLines("TestData.csv");
+
+
+        public static IEnumerable<IShape> Process(IEnumerable<string> input) =>
+            input.Select(line => line.Split(','))
+                 .Select(args => new KeyValuePair<string, double[]>(args[0],
+                                                                   new ArraySegment<string>(args, 1, args.Length - 1)
+                                                                        .Select(arg => double.Parse(arg, CultureInfo.InvariantCulture))
+                                                                        .ToArray()))
+                 .Select(shape => MyShapeFactory.CreateShape(shape.Key, shape.Value));
+    }
+}
