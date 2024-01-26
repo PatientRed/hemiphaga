07de2a3bc04afca400b617616d534bb1e973a65b
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:36:42 +0300

    Triangle > rightness calculation added

--- hemiphaga/hemiphaga/Shapes/Triangle.cs
+++ hemiphaga/hemiphaga/Shapes/Triangle.cs
@@ -6,6 +6,28 @@
         private readonly double _side2 = side2;
         private readonly double _side3 = side3;
 
+        private const double _precision = .001;
+
+        public bool IsRight
+        {
+            get
+            {
+                var sq1 = _side1 * _side1;
+                var sq2 = _side2 * _side2;
+                var sq3 = _side3 * _side3;
+
+                if (_side1 > _side2)
+                {
+                    if (_side1 > _side3)
+                        return Math.Abs(sq1 - sq2 - sq3) < _precision;
+                }
+                else if (_side2 > _side3)
+                    return Math.Abs(sq2 - sq1 - sq3) < _precision;
+
+                return Math.Abs(sq3 - sq1 - sq2) < _precision;
+            }
+        }
+
         public double CalculateArea()
         {
             //See Heron's formula
