ab5dc689ce98699bedc5a6d0dec215bee93be33a
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-26 20:39:21 +0300

    Dll client's new shapes implementation example added

---
+++ hemiphaga/PoW/MyNewShapesExtended.cs
@@ -0,0 +1,28 @@
+﻿using hemiphaga;
+
+//dll client extends basic functionality example
+namespace PoW.MyShapes
+{
+    internal class Square(double side) : IShape
+    {
+        private readonly double _side = side;
+
+        public double CalculateArea() => _side * _side;
+    }
+
+    internal static class MyShapeFactory
+    {
+        public static IShape CreateShape(string name, params double[] values)
+        {
+            switch (name)
+            {
+                case nameof(Square):
+                    return CreateSquare(values[0]);
+                default:
+                    return ShapeFactory.CreateShape(name, values);
+            }
+        }
+
+        private static Square CreateSquare(double side) => new Square(side);
+    }
+}
