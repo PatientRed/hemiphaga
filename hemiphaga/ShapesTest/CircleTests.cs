c65d730b97233ea7f707efebbdf329e4ee697975
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 16:56:22 +0300

    Circle tests implemented

---
+++ hemiphaga/ShapesTest/CircleTests.cs
@@ -0,0 +1,35 @@
+using hemiphaga;
+using System.Reflection;
+
+namespace ShapesTest
+{
+    [TestClass]
+    public class CircleTests
+    {
+        private const double _piArea = 1;
+
+        [TestMethod]
+        public void CircleCreationThrow()
+        {
+            var r = 0;
+
+            try
+            {
+                Helper.Construct(nameof(Circle), [r]);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType<TargetInvocationException>(ex);
+                Assert.IsInstanceOfType<ArgumentOutOfRangeException>(ex.InnerException);
+            }
+        }
+
+        [TestMethod]
+        public void CircleAreaGood()
+        {
+            var res = Helper.Construct(nameof(Circle), [_piArea]);
+
+            Assert.AreEqual(Math.PI, res.Area, Hemiphaga.Precision);
+        }
+    }
+}
