27511692270c9dba23ca3668170850775a0618db
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 18:02:35 +0300

    Simple factory custom figure adding test implemented

---
+++ hemiphaga/ShapesTest/SimpleFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿namespace hemiphaga.Tests
+{
+    internal class Rectangle : ICreatableShape<Rectangle>
+    {
+        public double Area => throw new NotImplementedException();
+
+        public static Rectangle Create(params double[] input)
+        {
+            return new Rectangle();
+        }
+    }
+
+    [TestClass]
+    public class SimpleFactoryTests
+    {
+        [TestMethod]
+        public void FactoryCustomFigureGoodTest()
+        {
+            Assert.IsInstanceOfType<Rectangle>(Helper.Construct(nameof(Rectangle), []));
+        }
+    }
+}
