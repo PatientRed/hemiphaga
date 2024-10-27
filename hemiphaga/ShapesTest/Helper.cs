5666928a8147b326c5fde0256dac7078337389db
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 16:56:10 +0300

    Helper test class added

---
+++ hemiphaga/ShapesTest/Helper.cs
@@ -0,0 +1,9 @@
+﻿using hemiphaga;
+
+namespace ShapesTest
+{
+    internal static class Helper
+    {
+        internal static IShape Construct(string figure, double[] dimensions) => SimpleShapeFactory.Create(figure, dimensions);
+    }
+}
