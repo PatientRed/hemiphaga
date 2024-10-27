904e3fbd4b79ef837be2543702bd0ddb9d9d71df
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 17:50:23 +0300

    Triangle.VerifyTriangleCorrectness() fixed

--- hemiphaga/hemiphaga/Shapes/Triangle.cs
+++ hemiphaga/hemiphaga/Shapes/Triangle.cs
@@ -47,7 +47,7 @@ namespace hemiphaga
             var sum = s1 + s2 + s3;
             double max = Math.Max(Math.Max(s1, s2), s3); ;
 
-            return Math.Abs(max + max - sum) < Precision; //max < sum - max
+            return max + max - sum < 0; //max < sum - max
         }
 
         public static Triangle Create(params double[] input) => new Triangle(input[0], input[1], input[2]);
