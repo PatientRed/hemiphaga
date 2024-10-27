61bcea33c435569d3ed8fb6f1a509d513043bdee
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 16:31:04 +0300

    Static helper class made public

--- hemiphaga/hemiphaga/hemiphaga.cs
+++ hemiphaga/hemiphaga/hemiphaga.cs
@@ -1,7 +1,7 @@
 ﻿namespace hemiphaga
 {
-    internal static class Hemiphaga
+    public static class Hemiphaga
     {
-        internal const double Precision = .000_001;
+        public const double Precision = .000_001;
     }
 }
