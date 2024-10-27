b061aa987f51aca534bbc8bfff38b076c0f5a564
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-27 17:46:57 +0300

    Helper test class added for triangle sides permutations

--- hemiphaga/ShapesTest/Helper.cs
+++ hemiphaga/ShapesTest/Helper.cs
@@ -5,5 +5,16 @@ namespace ShapesTest
     internal static class Helper
     {
         internal static IShape Construct(string figure, double[] dimensions) => SimpleShapeFactory.Create(figure, dimensions);
+
+        //TODO: use some algo (Heap's?) & generalize
+        internal static IEnumerable<double[]> PermutationsThree(double[] dimensions)
+        {
+            yield return dimensions;
+            yield return [dimensions[0], dimensions[2], dimensions[1]];
+            yield return [dimensions[1], dimensions[0], dimensions[2]];
+            yield return [dimensions[1], dimensions[2], dimensions[0]];
+            yield return [dimensions[2], dimensions[1], dimensions[0]];
+            yield return [dimensions[2], dimensions[0], dimensions[1]];
+        }
     }
 }
